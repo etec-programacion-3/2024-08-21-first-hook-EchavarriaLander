dddf560708187618e332316b153f3af0012abf3c
Lander <l.echavarria@alumno.etec.um.edu.ar>
2024-08-21 12:41:31 -0300

    First commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+const Counter = ({ count }) => {
+  return (
+    <Card className="text-center">
+      <Card.Body>
+        <Card.Title>Counter</Card.Title>
+        <Card.Text>
+          The current count is: <strong>{count}</strong>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Counter;
